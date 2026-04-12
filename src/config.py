96a8fc9cb94c40ee880d46a5b7ae9880ace0d869
Ali <ali2moh.04@gmail.com>
2026-04-12 23:26:20 +0300

    Ran E5: Dice Loss Function

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ IMAGE_SIZE = 256
 BATCH_SIZE = 8
 LEARNING_RATE = 1e-3
 NUM_EPOCHS = 20
-EXPERIMENT_ID = "E4"
-LOSS_TYPE = "bce"   # "bce" or "dice" or "bce_dice"
+EXPERIMENT_ID = "E5"
+LOSS_TYPE = "dice"   # "bce" or "dice" or "bce_dice"
 
 TRAIN_CSV = "data/processed/train.csv"
 VAL_CSV = "data/processed/val.csv"
