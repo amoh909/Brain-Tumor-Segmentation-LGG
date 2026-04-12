de7e3c6b61974a56c840f1ce0070d3bfda87010b
Ali <ali2moh.04@gmail.com>
2026-04-12 18:32:25 +0300

    Ran E3: Stronger Augmentation

--- src/augmentations.py
+++ src/augmentations.py
@@ -3,19 +3,20 @@ import albumentations as A
 
 def get_train_transforms():
     return A.Compose([
-        # A.HorizontalFlip(p=0.5),
-        # A.VerticalFlip(p=0.5),
-        # A.Affine(
-        #     translate_percent=0.06,
-        #     scale=(0.9, 1.1),
-        #     rotate=(-20, 20),
-        #     p=0.5
-        # ),
-        # A.RandomBrightnessContrast(
-        #     brightness_limit=0.2,
-        #     contrast_limit=0.2,
-        #     p=0.3
-        # ), for the sake of E2
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.Affine(
+            translate_percent=0.06,
+            scale=(0.9, 1.1),
+            rotate=(-20, 20),
+            p=0.5
+        ),
+        A.RandomBrightnessContrast(
+            brightness_limit=0.2,
+            contrast_limit=0.2,
+            p=0.3
+        ),
+        A.GaussNoise(p=0.2),
     ])
 
 
