851add4cf70ebba588a85b7928c7b90e90ab01a1
Ali <ali2moh.04@gmail.com>
2026-04-12 15:49:42 +0300

    Did E2: No Augmentation

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ IMAGE_SIZE = 256
 BATCH_SIZE = 8
 LEARNING_RATE = 1e-3
 NUM_EPOCHS = 20
-EXPERIMENT_ID = "E1"
+EXPERIMENT_ID = "E2"
 
 TRAIN_CSV = "data/processed/train.csv"
 VAL_CSV = "data/processed/val.csv"
